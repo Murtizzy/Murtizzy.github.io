0a99ad65343a19ae318ce6d4a131c0672f6427ba
Murtizzy <35986870+Murtizzy@users.noreply.github.com>
2020-02-06 20:32:35 -0700

    added calculator

---
+++ SENG513/a2/js/index.js
@@ -0,0 +1,44 @@
+
+function go(){
+    let display = document.getElementById("butt1");
+    let temp = display.value;
+    console.log(validExp());
+    if(validExp()){
+        display.style.backgroundColor = "white";
+        display.value = eval(display.value);
+    }
+    else{
+        display.style.backgroundColor = "#ee5253";
+        display.value = temp;
+    }
+}
+
+function show(button){
+    let butt = document.getElementById(button).innerHTML;
+    document.getElementById("butt1").value += butt; 
+}
+
+function del(){
+    let display = document.getElementById("butt1");
+    display.value = display.value.slice(0, -1)
+}
+
+function validExp(){
+    let display = document.getElementById("butt1").value;
+    for(let i=1; i<display.length;i++){
+       if(display[i]=='/'){
+           console.log(display.slice(i+1));
+            let denominator = eval(display.slice(i+1));
+            if(denominator != 0){
+                return true;
+            }
+            else{
+                return false;
+            }
+       }
+       else{
+           continue;
+       }
+    }
+    return true;
+}
